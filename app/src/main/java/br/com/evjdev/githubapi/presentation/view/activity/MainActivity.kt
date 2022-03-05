a9ded7095f8d77a70a8615c00222b6b612728f08
Emanoel Justino <emanoelvjj>
2022-03-05 00:29:16 -0300

    Added navigation librarie, nav graph file and adjusts

--- app/src/main/java/br/com/evjdev/githubapi/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/br/com/evjdev/githubapi/presentation/view/activity/MainActivity.kt
@@ -2,11 +2,20 @@ package br.com.evjdev.githubapi.presentation.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import br.com.evjdev.githubapi.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
