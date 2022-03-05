8a04049c0559f16cfa66d26a79801265b9e6a8c8
Emanoel Justino <emanoelvjj>
2022-03-04 21:15:05 -0300

    Created class to implementation's of gists repository and injected depency in constructor and some adjusts

--- app/src/main/java/br/com/evjdev/githubapi/data/api/GithubAPI.kt
+++ app/src/main/java/br/com/evjdev/githubapi/data/api/GithubAPI.kt
@@ -1,9 +1,10 @@
 package br.com.evjdev.githubapi.data.api
 
+import br.com.evjdev.githubapi.data.model.GistsResponse
 import retrofit2.http.GET
 
 interface GithubAPI {
 
     @GET
-    suspend fun getGists()
+    suspend fun getGists(): GistsResponse
 }
---
+++ app/src/main/java/br/com/evjdev/githubapi/data/repository/GistsRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package br.com.evjdev.githubapi.data.repository
+
+import br.com.evjdev.githubapi.data.api.GithubAPI
+import br.com.evjdev.githubapi.data.exception.GistsRepositoryException
+import br.com.evjdev.githubapi.data.model.toDomain
+import br.com.evjdev.githubapi.domain.model.Gists
+import java.lang.Exception
+import javax.inject.Inject
+
+class GistsRepositoryImpl @Inject constructor(
+    private val githubAPI: GithubAPI
+) : GistsRepository {
+
+    override suspend fun getGists(): Gists {
+        return try {
+            githubAPI.getGists().toDomain()
+
+        } catch (ex: Exception) {
+            throw GistsRepositoryException()
+        }
+    }
+}
