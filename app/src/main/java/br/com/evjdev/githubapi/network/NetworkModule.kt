67cc9c599a8b7e2b460d58c5c9afb7095c0f3d5c
Emanoel Justino <emanoelvjj>
2022-03-04 21:18:02 -0300

    created class networkmodule to provide singleton instance of service provider

---
+++ app/src/main/java/br/com/evjdev/githubapi/network/NetworkModule.kt
@@ -0,0 +1,14 @@
+package br.com.evjdev.githubapi.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    fun providesServiceProvider() = ServiceProvider()
+}
