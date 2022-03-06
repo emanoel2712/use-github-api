6d1cc8e730f9f481abeee194b3c2233412da1951
Emanoel Justino <emanoelvjj>
2022-03-05 21:52:16 -0300

    Implemented custom interceptor in retrofit of class Service Provider and Make provides dependency in hilt

--- app/src/main/java/br/com/evjdev/githubapi/network/NetworkModule.kt
+++ app/src/main/java/br/com/evjdev/githubapi/network/NetworkModule.kt
@@ -1,5 +1,7 @@
 package br.com.evjdev.githubapi.network
 
+import br.com.evjdev.githubapi.network.utils.InterceptorCustomImpl
+import br.com.evjdev.githubapi.network.utils.NetworkConnectionImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -7,8 +9,11 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-class NetworkModule {
+class NetworkModule() {
 
     @Provides
-    fun providesServiceProvider() = ServiceProvider()
+    fun providesServiceProvider(networkConnectionImpl: NetworkConnectionImpl): ServiceProvider {
+        val interceptorCustomImpl = InterceptorCustomImpl(networkConnectionImpl)
+        return ServiceProvider(interceptorCustomImpl)
+    }
 }
--- app/src/main/java/br/com/evjdev/githubapi/network/ServiceProvider.kt
+++ app/src/main/java/br/com/evjdev/githubapi/network/ServiceProvider.kt
@@ -1,19 +1,24 @@
 package br.com.evjdev.githubapi.network
 
+import br.com.evjdev.githubapi.network.utils.InterceptorCustom
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import javax.inject.Inject
 
-class ServiceProvider {
+class ServiceProvider @Inject constructor(interceptorCustom: InterceptorCustom) {
 
     private val baseUrl = "https://api.github.com/"
     private val contentType = "application/json".toMediaType()
     private val json = Json {
         ignoreUnknownKeys = true
+        isLenient = true
     }
 
-    private val retrofit = Retrofit.Builder().baseUrl(baseUrl)
+    private val okHttpClient = OkHttpClient.Builder().addInterceptor(interceptorCustom)
+    private val retrofit = Retrofit.Builder().client(okHttpClient.build()).baseUrl(baseUrl)
         .addConverterFactory(json.asConverterFactory(contentType)).build()
 
     fun <API> createService(apiClass: Class<API>): API = retrofit.create(apiClass)
